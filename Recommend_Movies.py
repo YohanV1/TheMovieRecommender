769733e905e378d30081dee593247d07757c86bb
Yohan Vinu <yohanvvinu@gmail.com>
2023-04-18 23:00:28 +0530

    Add session states

--- Recommend_Movies.py
+++ Recommend_Movies.py
@@ -5,12 +5,18 @@ import requests
 st.set_page_config(layout="wide")
 
 if 'movie_tracker' not in st.session_state:
-    st.session_state['movie_tracker'] = 0
+    st.session_state['movie_tracker'] = -1
 
-if st.session_state['movie_tracker'] == 0:
+if 'movie_index' not in st.session_state:
+    st.session_state['movie_index'] = 46
+
+if st.session_state['movie_tracker'] == -1:
     st.title('The Movie Recommender.')
 
-    option = st.selectbox('Select a movie.', (i for i in main.titles), index=46)
+    option = st.selectbox('Select a movie.', (i for i in main.titles),
+                          index=st.session_state['movie_index'])
+
+    st.session_state['movie_index'] = main.titles.index(option)
 
     option_img_response = requests.get(f'http://www.omdbapi.com/?t={option}&'
                                        f'apikey=2b84cbca')
@@ -35,7 +41,9 @@ if st.session_state['movie_tracker'] == 0:
 
     with col1:
         b1 = st.button(images[0][0], key=1)
-        st.session_state['movie_tracker'] += 1
+        if b1:
+            st.session_state['movie_tracker'] = 0
+            st.experimental_rerun()
         try:
             st.image(images[0][1]['Poster'], width=250)
         except KeyError:
@@ -44,7 +52,9 @@ if st.session_state['movie_tracker'] == 0:
 
     with col2:
         b2 = st.button(images[1][0], key=2)
-
+        if b2:
+            st.session_state['movie_tracker'] = 1
+            st.experimental_rerun()
         try:
             st.image(images[1][1]['Poster'], width=250)
         except KeyError:
@@ -53,7 +63,9 @@ if st.session_state['movie_tracker'] == 0:
 
     with col3:
         b3 = st.button(images[2][0], key=3)
-
+        if b3:
+            st.session_state['movie_tracker'] = 2
+            st.experimental_rerun()
         try:
             st.image(images[2][1]['Poster'], width=250)
         except KeyError:
@@ -64,7 +76,9 @@ if st.session_state['movie_tracker'] == 0:
 
     with col4:
         b4 = st.button(images[3][0], key=4)
-
+        if b4:
+            st.session_state['movie_tracker'] = 3
+            st.experimental_rerun()
         try:
             st.image(images[3][1]['Poster'], width=250)
         except KeyError:
@@ -73,7 +87,9 @@ if st.session_state['movie_tracker'] == 0:
 
     with col5:
         b5 = st.button(images[4][0], key=5)
-
+        if b5:
+            st.session_state['movie_tracker'] = 4
+            st.experimental_rerun()
         try:
             st.image(images[4][1]['Poster'], width=250)
         except KeyError:
@@ -82,7 +98,9 @@ if st.session_state['movie_tracker'] == 0:
 
     with col6:
         b6 = st.button(images[5][0], key=6)
-
+        if b6:
+            st.session_state['movie_tracker'] = 5
+            st.experimental_rerun()
         try:
             st.image(images[5][1]['Poster'], width=250)
         except KeyError:
@@ -93,7 +111,9 @@ if st.session_state['movie_tracker'] == 0:
 
     with col7:
         b7 = st.button(images[6][0], key=7)
-
+        if b7:
+            st.session_state['movie_tracker'] = 6
+            st.experimental_rerun()
         try:
             st.image(images[6][1]['Poster'], width=250)
         except KeyError:
@@ -102,7 +122,9 @@ if st.session_state['movie_tracker'] == 0:
 
     with col8:
         b8 = st.button(images[7][0], key=8)
-
+        if b8:
+            st.session_state['movie_tracker'] = 7
+            st.experimental_rerun()
         try:
             st.image(images[7][1]['Poster'], width=250)
         except KeyError:
@@ -111,7 +133,9 @@ if st.session_state['movie_tracker'] == 0:
 
     with col9:
         b9 = st.button(images[8][0], key=9)
-
+        if b9:
+            st.session_state['movie_tracker'] = 8
+            st.experimental_rerun()
         try:
             st.image(images[8][1]['Poster'], width=250)
         except KeyError:
@@ -119,6 +143,7 @@ if st.session_state['movie_tracker'] == 0:
         st.caption(f'{summaries[8]}')
 
 else:
-    st.write("hi")
-    print("hi")
-
+    b = st.button("Go back")
+    if b:
+        st.session_state['movie_tracker'] = -1
+        st.experimental_rerun()
